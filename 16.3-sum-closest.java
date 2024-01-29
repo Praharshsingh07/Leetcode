e73f9fa6f96b26d3727a993aaa48d028f2046a48
Praharshsingh <Praharsh.singh@s.amity.edu>
2024-01-29 21:23:45 +0530

    first commit

---
+++ 16.3-sum-closest.java
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=16 lang=java
+ *
+ * [16] 3Sum Closest
+ *
+ * https://leetcode.com/problems/3sum-closest/description/
+ *
+ * algorithms
+ * Medium (45.65%)
+ * Likes:    10188
+ * Dislikes: 537
+ * Total Accepted:    1.2M
+ * Total Submissions: 2.5M
+ * Testcase Example:  '[-1,2,1,-4]\n1'
+ *
+ * Given an integer array nums of length n and an integer target, find three
+ * integers in nums such that the sum is closest to target.
+ * 
+ * Return the sum of the three integers.
+ * 
+ * You may assume that each input would have exactly one solution.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [-1,2,1,-4], target = 1
+ * Output: 2
+ * Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [0,0,0], target = 1
+ * Output: 0
+ * Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 =
+ * 0).
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 3 <= nums.length <= 500
+ * -1000 <= nums[i] <= 1000
+ * -10^4 <= target <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        
+    }
+}
+// @lc code=end
+
